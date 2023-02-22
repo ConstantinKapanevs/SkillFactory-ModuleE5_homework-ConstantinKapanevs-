02e37aa02966fee4b6ed5cb85049ab3f50ac9297
ConstantinKapanevs <2053240@mail.ru>
2023-02-22 14:01:35 +0300

    Button stylization and pictures simple animation is added...

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let currentImage = document.querySelector('main').firstElementChild
 
+const previousPicture = function () {
+    if (currentImage.previousElementSibling) {
+        currentImage.hidden = true
+        currentImage = currentImage.previousElementSibling
+        currentImage.hidden = false
+    } else {
+        currentImage.hidden = true
+        currentImage = document.querySelector('main').lastElementChild
+        currentImage.hidden = false
+    }
+
+}
 const nextPicture = function () {
     if (currentImage.nextElementSibling) {
-        currentImage.nextElementSibling.hidden = false
-        currentImage.hidden = true       
+        currentImage.hidden = true;
+
         currentImage = currentImage.nextElementSibling
+        currentImage.hidden = false
     } else {
         currentImage.hidden = true
         currentImage = document.querySelector('main').firstElementChild
@@ -14,17 +27,6 @@ const nextPicture = function () {
 }
 
 
-const previousPicture = function () {
-    if (currentImage.previousElementSibling) {
-        currentImage.previousElementSibling.hidden = false
-        currentImage.hidden = true       
-        currentImage = currentImage.previousElementSibling
-    } else {
-        currentImage.hidden = true
-        currentImage = document.querySelector('main').lastElementChild
-        currentImage.hidden = false
-    }
 
-}
-forward.addEventListener('click', nextPicture)
 back.addEventListener('click', previousPicture)
+forward.addEventListener('click', nextPicture)
